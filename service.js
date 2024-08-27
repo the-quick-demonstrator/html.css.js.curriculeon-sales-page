e038adfd075f7a506682938cba269d28d61a0fb1
git-leon <xleonhunter@gmail.com>
2024-08-27 17:38:37 -0400

    updated to ensure skillset can now toggle-slide

--- service.js
+++ service.js
@@ -1,3 +1,33 @@
-document.getElementById('enroll-button').addEventListener('click', function() {
-    document.getElementById('ezuz1x').click();
-});
+class Configuration {
+    enableEnrollmentRedirect() {
+        document.getElementById('enroll-button').addEventListener('click', function() {
+            document.getElementById('ezuz1x').click();
+        });
+    }
+
+
+    enableHideableSkillListJquery() {
+        $(document).ready(function(){
+            $('.reveal-button').click(function(){
+                $('.skills-list').slideToggle();
+            });
+        });
+    }
+
+    enableHideableSkillList() {
+        document.addEventListener('DOMContentLoaded', function() {
+            document.querySelector('.reveal-button').addEventListener('click', function() {
+                var skillsList = document.querySelector('.skills-list');
+                if (skillsList.style.display === 'none' || skillsList.style.display === '') {
+                    skillsList.style.display = 'block';
+                } else {
+                    skillsList.style.display = 'none';
+                }
+            });
+        });
+    }
+}
+
+const configuration = new Configuration();
+configuration.enableEnrollmentRedirect();
+configuration.enableHideableSkillListJquery();
